7ef48d0f1c028d9095055d650e1313929f726cf6
tikhonsmovzh <81905988+tikhonsmovzh@users.noreply.github.com>
2023-01-19 20:42:22 +0300

    Рисование по таймеру

--- Ship_Debbuger/Ship_Debbuger/CompasCalibrate.xaml.cs
+++ Ship_Debbuger/Ship_Debbuger/CompasCalibrate.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using Xamarin.Forms;
@@ -15,25 +16,54 @@ namespace Ship_Debbuger
     {
         private SKBitmap _paletteBitmap = null;
         private SKBitmap PaletteBitmap => _paletteBitmap ?? (_paletteBitmap = new SKBitmap((int)_canvasView.Width, (int)_canvasView.Height));
+        private readonly Timer _timer;
+        private readonly Random _random;
         public CompasCalibrate()
         {
             InitializeComponent();
             _startButton.Text =  "Начать";
+            _timer = new Timer
+            {
+                Interval = 500
+            };
+            _timer.Elapsed += _timer_Elapsed;
+            _random = new Random();
         }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+
+            using (SKCanvas canvas = new SKCanvas(PaletteBitmap))
+            {
+
+                canvas.DrawCircle(_random.Next(PaletteBitmap.Width-100)+50, _random.Next(PaletteBitmap.Height - 100) + 50, 50, new SKPaint() { Color = new SKColor(255, 0, 0), IsStroke = true });
+                //.DrawRect(0, 0, 50, 50, new SKPaint() { Color = new SKColor(255, 0,0) });
+                _canvasView.InvalidateSurface();
+            }
+        }
+
         private bool _isWorking;
         private void Button_Clicked(object sender, EventArgs e)
         {
             _isWorking = !_isWorking;
             _startButton.Text = _isWorking ? "Остановить" : "Начать";
-
-
-            using (SKCanvas canvas = new SKCanvas(PaletteBitmap))
+            if (_isWorking)
             {
-
-                canvas.DrawCircle(200, 300, 50, new SKPaint() { Color = new SKColor(255, 0, 0), IsStroke = true });
-                //.DrawRect(0, 0, 50, 50, new SKPaint() { Color = new SKColor(255, 0,0) });
-                _canvasView.InvalidateSurface();
+                _timer.Start();
             }
+            else
+            {
+                _timer.Stop();
+            }
+
+
+            //using (SKCanvas canvas = new SKCanvas(PaletteBitmap))
+            //{
+
+            //    canvas.DrawCircle(200, 300, 50, new SKPaint() { Color = new SKColor(255, 0, 0), IsStroke = true });
+            //    //.DrawRect(0, 0, 50, 50, new SKPaint() { Color = new SKColor(255, 0,0) });
+            //    _canvasView.InvalidateSurface();
+            //}
 
         }
 
